08ffb7e5dda66e06730e124d8c03906e954a4699
Shridhar-dev <shridharkamat10@gmail.com>
2020-12-13 15:17:08 +0530

    first one

--- src/Footer.js
+++ src/Footer.js
@@ -8,10 +8,10 @@ function Footer(props) {
                     <div class="col-12 col-md-6 col-xl-6 offset-0 offset-md-6 offset-xl-6 mb-5 mb-md-0 mb-xl-0" style={{height:'25rem'}}>
                    <div class="mt-0 mt-md-5 mt-xl-5 mb-5 mb-md-0 mb-xl-0 " style={{color:props.bg}}>
                     <h1 class="m-0 p-0 pb-5 mt-0 mt-md-5 mt-xl-5" style={{fontSize:'4rem'}}>Join our <strike>cult</strike> family </h1>
-                   <a href="https://twitter.com/shridhar_kdev"> <i class="fab fa-twitter mx-3" style={{fontSize:'4rem'}}></i></a>
-                   <a href="https://www.youtube.com/channel/UCv2hTdClW8560n89OZ6cazA?view_as=subscriber"> <i class="fab fa-youtube mx-3" style={{fontSize:'4rem'}}></i></a>
-                   <a href="https://github.com/Shridhar-dev"> <i class="fab fa-github mx-3" style={{fontSize:'4rem'}}></i></a>
-                   <a href="https://www.linkedin.com/in/shridhar-kamat-1015a41bb/" style={{textDecoration:'none'}}> <i class="fab fa-linkedin mx-3" style={{fontSize:'4rem'}}></i></a>
+                   <a href="https://twitter.com/shridhar_kdev"> <i class="fab fa-twitter mx-3" style={{fontSize:'4rem',color:props.bg}}></i></a>
+                   <a href="https://www.youtube.com/channel/UCv2hTdClW8560n89OZ6cazA?view_as=subscriber"> <i class="fab fa-youtube mx-3" style={{fontSize:'4rem',color:props.bg}}></i></a>
+                   <a href="https://github.com/Shridhar-dev"> <i class="fab fa-github mx-3" style={{fontSize:'4rem',color:props.bg}}></i></a>
+                   <a href="https://www.linkedin.com/in/shridhar-kamat-1015a41bb/" style={{textDecoration:'none'}}> <i class="fab fa-linkedin mx-3" style={{fontSize:'4rem',color:props.bg}}></i></a>
                     </div>
                     </div>
                 </div>
--- src/Item.js
+++ src/Item.js
@@ -6,9 +6,9 @@ function Item(props) {
         <div class="col-12 col-md-6 col-xl-4 my-4 " >
             <a href={props.url} target="_blank">
             <div class="pro-img-div d-flex justify-content-center align-items-center flex-column" style={{color:props.icolor,backgroundColor:'transparent'}}>
-                <div  class="mx-5">
-                <h2 class="mb-3">{props.heading}</h2>
-                <p >{props.text}</p>
+                <div  class="mx-5 ">
+                <h2 class="mb-3 mx-auto">{props.heading}</h2>
+                <p class="mx-auto">{props.text}</p>
                 </div>
                 
             <img src={props.img} class="img-fluid pro-img" width="400" alt="..." style={{border:'1px solid white'}}/>
